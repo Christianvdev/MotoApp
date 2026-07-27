f40ed91b705e8c61af6af5d83f2d199d1cea9ab2
Christian Vaz <Christiantech77@gmail.com>
2026-07-26 20:32:55 -0400

    Updated settings with secret key being moved.

--- MotoApp/MotoTracker/settings.py
+++ MotoApp/MotoTracker/settings.py
@@ -15,12 +15,15 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+from dotenv import load_dotenv
+BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / ".env")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s$+mo&xufp@1t63q1d+i2g89g^&8f$&-^p-*k$@^@tga5()2ap'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
