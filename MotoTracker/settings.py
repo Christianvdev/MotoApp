bf79f007b0e185e9661ee095b1f89f8876bb2262
Christian Vaz <Christiantech77@gmail.com>
2026-07-26 22:32:31 -0400

    fixed newly added CORS url

--- MotoTracker/settings.py
+++ MotoTracker/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:4173",
-    "motoapp-frontend.netlify.app",
+    "https://motoapp-frontend.netlify.app",
 ]
 
 REST_FRAMEWORK = {
