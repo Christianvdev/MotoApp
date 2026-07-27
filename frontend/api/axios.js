3ee170eeb92389aaaa31ff0a2bb64d348c1f9d6c
Christian Vaz <Christiantech77@gmail.com>
2026-07-26 22:50:13 -0400

    removed redirect for testing

--- frontend/api/axios.js
+++ frontend/api/axios.js
@@ -52,8 +52,6 @@ api.interceptors.response.use(
 
                 localStorage.removeItem('access_token')
                 localStorage.removeItem('refresh_token')
-
-                window.location.href = '/'
             }
         }
 
