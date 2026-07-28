85f4da3a3fb3233d45871910d5e11ff46846730e
Christian Vaz <Christiantech77@gmail.com>
2026-07-27 23:21:38 -0400

    Fixed capital character for netlify development

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import api from "../../api/axios"
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
-import '../styles/auth.css'
+import '../styles/Auth.css'
 
 const LoginPage = () => {
     const [username, setUsername] = useState('')
