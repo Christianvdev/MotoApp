fec28031d93ee7ac7b2eccf2e97b178aff9cf82d
Christian Vaz <Christiantech77@gmail.com>
2026-08-02 19:27:17 -0400

    updated placeholder url

--- MotoTracker/settings.py
+++ MotoTracker/settings.py
@@ -142,5 +142,5 @@ ALLOWED_HOSTS = [
 
 CSRF_TRUSTED_ORIGINS = [
     "https://motoapp-frontend.netlify.app",
-    'https://your-app-name.onrender.com',
+    'https://motoapp-backend-trtv.onrender.com',
 ]
